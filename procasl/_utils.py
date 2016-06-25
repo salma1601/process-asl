527122a8d77a42a97613a13674589a3c540c5b6c
Salma <salmabougacha@hotmail.com>
2016-06-26 00:20:31 +0200

    scipy savemat doesnot handle none

--- procasl/_utils.py
+++ procasl/_utils.py
@@ -1,7 +1,9 @@
 import os
+import copy
 import warnings
 
 import numpy as np
+from scipy import io
 import nibabel
 
 
@@ -118,3 +120,207 @@ def fill_nan(in_file, fill_value=0.):
     out_file += '_no_nan.nii'
     nibabel.save(img, out_file)
     return out_file
+
+
+def _compute_depth(dictionary):
+    if not isinstance(dictionary, dict):
+        raise ValueError('Can not compute depth of non dictionary type.'
+        'Data has type {0}'.format(dictionary))
+
+    depths = []
+    for value in dictionary.itervalues():
+        if isinstance(value, dict):
+            depth = 1 + _compute_depth(value)
+        else:
+            depth = 0
+        depths.append(depth)
+    if depths:
+        depth = np.max(depths)
+    else:
+        depth = 0
+
+    return depth
+
+
+def _deepest_dict_to_rec(dictionary):
+    """transforms the deepest dictionary to a recarray
+    """
+    if not isinstance(dictionary, dict):
+        raise ValueError('Can not convert non dictionary type.'
+                         'Data has type {0}'.format(dictionary))
+
+    if dictionary == {}:
+        return dictionary
+
+    dictionary_out = copy.copy(dictionary)
+    depth = _compute_depth(dictionary)
+    actual_depth = -1
+    for key, value in dictionary.iteritems():
+        if isinstance(value, dict):
+            actual_depth += 1
+            dictionary_out[key] = _deepest_dict_to_rec(value)
+            if actual_depth == depth:
+                dictionary_out_keys, dictionary_out_values = \
+                    zip(*dictionary_out.items())
+                dtype = zip(dictionary_out_keys,
+                              [type(key) for key in dictionary_out_keys])
+                dtype = zip(dictionary_out_keys,
+                              [type(value) for value in dictionary_out_values])
+                dictionary_out = np.array([tuple(dictionary_out_values), ],
+                                           dtype=dtype)
+    return dictionary_out
+
+
+def squeeze_data(mat_struct):
+    data = copy.copy(mat_struct)
+    if isinstance(data, np.void):
+        for name in data.dtype.names:
+            if isinstance(name, str):
+                print name
+            data[name] = squeeze_data(data[name])
+    elif isinstance(data, dict):
+        for name in data.keys():
+            if isinstance(name, str):
+                print name
+            data[name] = squeeze_data(data[name])
+    elif isinstance(data, io.matlab.mio5_params.MatlabObject) or\
+            isinstance(data, np.ndarray):
+        if data.shape == (1, 1):
+            data = squeeze_data(data[0, 0])
+        elif data.dtype.fields is not None:
+            for name in data.dtype.names:
+                if isinstance(name, str):
+                    print name
+                data[name] = squeeze_data(data[name])
+        else:
+            for n, _ in enumerate(data):
+                data[n] = squeeze_data(data[n])
+
+    return data
+
+
+def remove_none(mat_struct, recarray=False):
+    data = copy.copy(mat_struct)
+    if isinstance(data, np.void):
+        for name in data.dtype.names:
+            data[name] = remove_none(data[name])
+    elif isinstance(data, dict):
+        for name in data.keys():
+            data[name] = remove_none(data[name])
+    elif isinstance(data, io.matlab.mio5_params.MatlabObject) or\
+            isinstance(data, np.ndarray):
+        if data.dtype.fields is not None:
+            recarray = True
+            for name in data.dtype.names:
+                if isinstance(name, str):
+                    print name
+                data[name] = remove_none(data[name], recarray=True)
+        else:
+            if not recarray and np.all(data == np.array([[None]], dtype='O')):
+                data = np.array([[]])
+            else:
+                for n, _ in enumerate(data):
+                    data[n] = remove_none(data[n], recarray=recarray)
+    elif data is None:
+        data = []
+
+    return data
+
+
+def void_to_dict(mat_struct):
+    """Transforms a nmpy.void to dictionary
+    """
+    data = copy.copy(mat_struct)
+#    print type(data)
+    if isinstance(data, np.void):
+        dictionary = {}
+        for name in data.dtype.names:
+            dictionary[name] = void_to_dict(data[name])
+    elif isinstance(data, io.matlab.mio5_params.MatlabObject):
+#        if np.shape(data) == (1, 1):
+#            dictionary = void_to_dict(data[0, 0])
+        if data.dtype.hasobject:
+            if data.dtype.fields is not None and data.size == 1:
+                dictionary = {}
+                for name in data.dtype.names:
+                    if isinstance(name, str):
+                        print name
+                    dictionary[name] = void_to_dict(data[name])
+            else:
+                dictionary = void_to_dict(data[0])
+    elif isinstance(data, np.ndarray) and np.shape(data) == (1, 1):
+        dictionary = void_to_dict(data[0, 0])
+    elif isinstance(data, np.ndarray) and data.dtype.hasobject:
+        if data.dtype.fields is not None:
+            dictionary = {}
+            for name in data.dtype.names:
+                if isinstance(name, str):
+                    print name
+                dictionary[name] = void_to_dict(data[name])
+        else:
+            dictionary = np.empty(shape=data.shape, dtype=data.dtype)
+            for n, sub_data in enumerate(data):
+                if data.shape == (164, 1) and (n == 0 or n == 100):
+                    print "_" * 10
+                    print n, sub_data
+                x = void_to_dict(sub_data)
+                dictionary[n] = x
+#            dictionary = dictionary.reshape(data.shape)
+    elif data is None:
+        print '*' * 25
+        dictionary = np.array([[]], dtype=object)
+    else:
+        dictionary = data
+
+    if isinstance(dictionary, dict):
+        dictionary = _deepest_dict_to_rec(dictionary)
+    if isinstance(dictionary, np.ndarray):
+        dictionary = np.squeeze(dictionary)
+
+    return dictionary
+
+
+def _check_structured_data_equal(data1, data2):
+    if isinstance(data1, dict) and isinstance(data2, dict):
+        set1 = set(data1.keys())
+        set2 = set(data2.keys())
+        if set1 == set2:
+            checks = []
+            for key in set1:
+                print key
+                checks.append(_check_structured_data_equal(data1[key],
+                                                           data2[key]))
+            check = np.all(checks)
+        else:
+            message = ''
+            if set1 - set2:
+                message.extend('Keys {0} not found in data 2. '.format(
+                    set1 - set2))
+            if set2 - set1:
+                message.extend('Keys {0} not found in data 1. '.format(
+                    set2 - set1))
+            print(message)
+            return False
+    elif (isinstance(data1, np.ndarray) and isinstance(data2, np.ndarray)) or\
+         (isinstance(data1, np.void) and isinstance(data2, np.void)):
+        if data1.shape == data2.shape:
+            print data1.shape
+            dtype1 = data1.dtype
+            dtype2 = data2.dtype
+            if set(dtype1.descr) == set(dtype2.descr):
+                check = np.all(data1 == data2)
+            else:
+                print('data1 has dtype {0}, data2 {1}'.format(
+                    data1.dtype, data2.dtype))
+                return False
+        else:
+            print('data1 has shape {0}, data2 {1}'.format(
+                data1.shape, data2.shape))
+            return False
+    elif type(data1) == type(data2):
+        check = (data1 == data2)
+    else:
+        print('data& has type {1}, data2 has type {2}'.format(
+            type(data1), type(data2)))
+        return False
+    return check
--- procasl/tests/test_utils.py
+++ procasl/tests/test_utils.py
@@ -1,6 +1,8 @@
+import os
 import numpy as np
 from numpy.testing import assert_array_equal
-from nose.tools import assert_raises
+from scipy.io import loadmat, savemat
+from nose.tools import assert_raises, assert_equal
 import nibabel
 
 from procasl import _utils
@@ -61,3 +63,93 @@ def test_fill_nan():
     data[1, 4, 1] = fill_value
     data[2, 3, 2] = fill_value
     assert_array_equal(nibabel.load(out_file).get_data(), data)
+
+
+def test_compute_depth():
+    dictionary1 = {'name': 'Thomas', 'age': 7.5}
+    assert_equal(_utils._compute_depth(dictionary1), 0)
+    dictionary2 = {'name': 'Thomas', 'age': 7.5, 'dict1': dictionary1}
+    assert_equal(_utils._compute_depth(dictionary2), 1)
+    dictionary2 = {'name': 'Thomas', 'dict1': dictionary1,
+                   'dict2': dictionary1}
+    assert_equal(_utils._compute_depth(dictionary2), 1)
+    dictionary3 = {'name': 'Thomas', 'dict1': dictionary1,
+                   'dict2': dictionary2}
+    assert_equal(_utils._compute_depth(dictionary3), 2)
+
+
+# TODO: create input correctly, as in nilearn.datasets
+def test_void_to_dict():
+    # Simple dict, simple values
+    filename = '/tmp/test_file.mat'
+    dictionary_in = {'name': 'Thomas', 'age': 7.5}
+    savemat(filename, dictionary_in)
+    mat_data = loadmat(filename, struct_as_record=True, squeeze_me=False)
+    dictionary_out = _utils.void_to_dict(mat_data)
+    assert(dictionary_out['name'] == 'Thomas')
+    assert(dictionary_out['age'] == [7.5])
+
+    # Simple dict, complex values
+    dictionary_in = {'name': 'Thomas', 'age': np.array([[7.5]])}
+    savemat(filename, dictionary_in)
+    mat_data = loadmat(filename, struct_as_record=True, squeeze_me=False)
+    dictionary_out = _utils.void_to_dict(mat_data)
+    assert(dictionary_out['name'] == 'Thomas')
+    assert(dictionary_out['age'] == np.array([[7.5]]))
+
+    # Dict of dict
+    dictionary_in = {'agent': {'name': 'Thomas', 'age': np.array([[7.5]])},
+                     'building': ['beautiful', 'expensive']}
+    savemat(filename, dictionary_in)
+    mat_data = loadmat(filename, struct_as_record=True, squeeze_me=True)
+    dictionary_out = _utils.void_to_dict(mat_data)
+    assert(np.size(dictionary_out['building']) == 2)
+    assert(dictionary_out['building'][0] == 'beautiful')
+    assert(dictionary_out['building'][1] == 'expensive')
+    assert(dictionary_out['agent']['name'] == 'Thomas')
+    assert(dictionary_out['agent']['age'] == 7.5)
+
+    # Dict of dict of dict
+    dictionary_in = {'dict1': dictionary_in, 'depth': 2}
+    savemat(filename, dictionary_in)
+    mat_data = loadmat(filename, struct_as_record=True, squeeze_me=True)
+    dictionary_out = _utils.void_to_dict(mat_data)
+    sub_dict = dictionary_out['dict1']
+    assert(str(sub_dict['building']) == "[u'beautiful' u'expensive']")
+    assert(str(sub_dict['agent']) == "(7.5, u'Thomas')")
+    os.remove(filename)
+
+
+def test_check_structured_data_equal():
+    dictionary1 = {'agent': {'name': 'Thomas', 'age': np.array([[7.5]])},
+                   'building': ['beautiful', 'expensive']}
+    assert(_utils._check_structured_data_equal(dictionary1,
+                                               dictionary1.copy()))
+    array1 = np.random.rand(34, 12, 2)
+    assert(_utils._check_structured_data_equal(array1, array1.copy()))
+    dictionary2 = {'agent': {'name': 'Thomas',
+                             'age': np.array([[7.5]], dtype='O')},
+                   'building': ['beautiful', 'expensive']}
+    assert(not (_utils._check_structured_data_equal(dictionary1, dictionary2)))
+
+
+def test_remove_none():
+    array = np.array([[None]], dtype='O')
+    nan_array = np.array([[np.nan]], dtype=float)
+    assert_array_equal(_utils.remove_none(array), nan_array)
+
+    dictionary = {'field1': 2.3, 'field2': array}
+    dictionary_out = _utils.remove_none(dictionary)
+    assert_equal(set(dictionary_out.keys()), {'field1', 'field2'})
+    assert_equal(dictionary_out['field1'], 2.3)
+    assert_array_equal(dictionary_out['field2'], nan_array)
+
+    rec_array = np.array([(3, 2.1, array), ],
+                         dtype=[('field1', np.int), ('field2', np.float),
+                                ('field3', np.ndarray)])
+    rec_array_out = _utils.remove_none(rec_array)
+    assert_equal(rec_array_out['field1'], 3)
+    assert_equal(rec_array_out['field2'], 2.1)
+    assert_array_equal(rec_array['field3'][0], array)
+    assert_array_equal(rec_array_out['field3'][0],
+                       np.array([[np.nan]], dtype='O'))
